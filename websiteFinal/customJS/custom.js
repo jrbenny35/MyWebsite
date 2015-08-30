b710226e25ff2b73f105adffb7002053df714e85
jrbenny35 <bennyjr169@gmail.com>
2015-08-30 13:22:52 -0400

    Added window width to JS for correct scrolling for mobile. Will probably refine

--- websiteFinal/customJS/custom.js
+++ websiteFinal/customJS/custom.js
@@ -9,9 +9,12 @@ $(document).ready(function(){
     $('.navbar .nav .nav-item .nav-link').on('click', function(){
 
         var whereToScroll = $(this).attr('data-scroll');
+        var windowSize = $(window).width();
 
-        $.scrollTo(document.getElementById(whereToScroll), 700, {offset:-80});
 
+        if(windowSize < 800) {
+            $.scrollTo(document.getElementById(whereToScroll), 700, {offset: -125});
+        } else{$.scrollTo(document.getElementById(whereToScroll), 700, {offset: -80});}//end if
 
     });//End function
 
